beb790da13d26e495d9942df08152d95a6154f8c
Cyber Angel <cyber@mj00.top>
2025-05-02 15:48:32 +1200

    Add player list

---
+++ assets/js/bailu2players.js
@@ -0,0 +1,9 @@
+function displayCaptains() {
+    document.getElementById("captains").style.display = "block";
+    document.getElementById("players").style.display = "none";
+}
+
+function displayPlayers() {
+    document.getElementById("captains").style.display = "none";
+    document.getElementById("players").style.display = "block";
+}
