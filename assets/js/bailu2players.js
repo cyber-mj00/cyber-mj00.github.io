3853c1ef08d06f9b5ea7e6c47551c83ef5825aac
Cyber Angel <cyber@mj00.top>
2025-07-25 13:17:01 +1200

    Updates

--- assets/js/bailu2players.js
+++
@@ -1,9 +0,0 @@
-function displayCaptains() {
-    document.getElementById("captains").style.display = "block";
-    document.getElementById("players").style.display = "none";
-}
-
-function displayPlayers() {
-    document.getElementById("captains").style.display = "none";
-    document.getElementById("players").style.display = "block";
-}
